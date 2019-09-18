14603145062bffc90708f1d6d5a3bbf162a7b235
Andrew Cordiner <andrewcordiner@Andrews-MacBook-Pro.local>
2019-09-18 23:10:17 +1000

    Added ex_doc

--- credo_module_function_ordering/mix.exs
+++ credo_module_function_ordering/mix.exs
@@ -28,7 +28,8 @@ defmodule CredoModuleFunctionOrdering.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:credo, "~> 1.1.0", only: [:dev, :test], runtime: false}
+      {:credo, "~> 1.1.0", only: [:dev, :test], runtime: false},
+      {:ex_doc, "~> 0.21.2", only: :dev, runtime: false}
     ]
   end
 end
